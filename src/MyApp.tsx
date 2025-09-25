31500d0e050ef8bb38f7c55e8e74f656781a2ab4
fjrd12 <fjrd12@users.noreply.github.com>
2025-09-25 15:28:34 -0600

    Add subtitle button

--- src/MyApp.tsx
+++ src/MyApp.tsx
@@ -1,8 +1,28 @@
-import { Card, CardHeader, Text, Icon } from '@ui5/webcomponents-react';
+import {
+  Avatar,
+  Card,
+  CardHeader,
+  Text,
+  ShellBar,
+  ShellBarItem,
+  List,
+  ListItemStandard,
+  ListItemCustom,
+  ProgressIndicator,
+  FlexBox,
+  FlexBoxJustifyContent,
+  FlexBoxWrap,
+  FlexBoxDirection,
+  AnalyticalTable,
+  Icon,
+} from '@ui5/webcomponents-react';
+import activateIcon from '@ui5/webcomponents-icons/dist/activate.js';
 import { BarChart, LineChart } from '@ui5/webcomponents-react-charts';
 import lineChartIcon from '@ui5/webcomponents-icons/dist/line-chart.js';
 import barChartIcon from '@ui5/webcomponents-icons/dist/horizontal-bar-chart.js';
 import { useState } from 'react';
+import reactLogo from './assets/reactLogo.png';
+import profilePictureExample from './assets/profilePictureExample.png';
 
 export function MyApp() {
   const [toggleCharts, setToggleCharts] = useState('lineChart');
@@ -61,6 +81,18 @@ export function MyApp() {
 
   return (
     <div>
+      <ShellBar
+        logo={<img src={reactLogo} alt="Company Logo" />}
+        profile={
+          <Avatar>
+            <img src={profilePictureExample} alt="User Avatar" />
+          </Avatar>
+        }
+        primaryTitle="My App"
+      >
+        <ShellBarItem icon={activateIcon} text="Activate" />
+      </ShellBar>
+
       <Card
         header={
           <CardHeader
